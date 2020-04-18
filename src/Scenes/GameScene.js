baeee8593fa7df0f6bf37e366e59995276f8cac1
Kimbsy <lordkimber@gmail.com>
2020-04-18 21:53:53 +0100

    cleaning up

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -11,46 +11,49 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create () {
-        var config = this.game.config;
         this.model = this.sys.game.globals.model;
-
+        var config = this.game.config;
         var matter = this.matter
         matter.world.setBounds(0, 0, game.config.width, game.config.height);
+        matter.add.mouseSpring();
+        this.addBalloon();
+    }
+
+    addBalloon(matter) {
+        var matter = this.matter
 
         var balloon = matter.add.image(400, 200, 'balloon', null, {
-          shape: {type: 'circle',
-                  radius: 32},
-          mass: 0.1,
-          ignorePointer: true,
-          gravityScale: {x: 1, y: -1}
+            shape: {type: 'circle',
+                    radius: 32},
+            mass: 0.1,
+            ignorePointer: true,
+            gravityScale: {y: -1}
         });
-
-        var firstRope = this.matter.add.sprite(400, 231.5, 'rope', null, {
-          ignoreGravity: true,
-          ignorePointer: true
-        });
-        matter.add.joint(balloon, firstRope, 35);
-
-        var prev = firstRope;
-        var i;
-        for (i = 0; i < 10; i++) {
-          var ropeSection = this.matter.add.image(400, 241.5 + (i * 10), 'rope', null, {
+  
+        var firstRopeSection = matter.add.image(400, 200, 'rope', null, {
             ignoreGravity: true,
             ignorePointer: true
-          });
-          matter.add.joint(prev, ropeSection, 13);
-          prev = ropeSection;
+        });
+        matter.add.joint(balloon, firstRopeSection, 0, 1, {pointA: {x: 0, y: 35}});
+
+        var prev = firstRopeSection;
+        var segmentCount = 8
+        for (var i = 0; i < segmentCount; i++) {
+            var ropeSection = matter.add.image(400, 235 + (i * 15), 'rope', null, {
+                ignoreGravity: true,
+                ignorePointer: true
+            });
+            matter.add.joint(prev, ropeSection, 15);
+            prev = ropeSection;
         }
 
-        var lastRope = this.matter.add.image(400, 251.5 + (i * 10), 'rope', null, {
-          mass: 50000,
-          ignoreGravity: true,
-          frictionAir: 1,
-          fixedRotation: true
+        var lastRopeSection = matter.add.image(400, 235 + ((i + 1) * 15), 'rope', null, {
+            mass: 50000,
+            ignoreGravity: true,
+            frictionAir: 1,
+            fixedRotation: true
         });
-        matter.add.joint(prev, lastRope, 20);
-
-        matter.add.mouseSpring();
+        matter.add.joint(prev, lastRopeSection, 15);
     }
 
     update () {
@@ -58,4 +61,3 @@ export default class GameScene extends Phaser.Scene {
         this.model = this.sys.game.globals.model; 
     }
 };
-
